0f1dfc1850eefadb0ae30e9e644ec55e7e043254
schriftgestalt <georg.seifert@mac.com>
2026-01-15 18:18:06 +0100

    remove unneeded code

--- BubbleKernCentral.glyphsPlugin/Contents/Resources/plugin.py
+++ BubbleKernCentral.glyphsPlugin/Contents/Resources/plugin.py
@@ -4,39 +4,5 @@
 # IT'S CALLED BUBBLEKERN '4' FOR INTERNAL PURPOSE ONLY.
 # I'LL CHANGE '4' TO 2 EVENTUALLY.
 
-from __future__ import division, print_function, unicode_literals
-import objc
-from GlyphsApp import Glyphs, EDIT_MENU
-from GlyphsApp.plugins import GeneralPlugin
-from AppKit import NSMenuItem
-
 from BKKerner import BubbleKernKerner
-from BKReporter import ShowKernBubbles4
 from BKTool import BubbleKernTool4
-
-class BubbleKern4(GeneralPlugin):
-
-	@objc.python_method
-	def settings(self):
-		self.name = Glyphs.localize({
-			'en': 'BubbleKern 4',
-		})
-
-	@objc.python_method
-	def start(self):
-		self.reporter = ShowKernBubbles4.alloc().init()
-		self.tool = BubbleKernTool4.alloc().init()
-		# mainMenu = Glyphs.mainMenu()
-		self.kerner = BubbleKernKerner.alloc().init()
-		# newMenuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(self.name, self.showWindow_, "")
-		# newMenuItem.setTarget_(self)
-		# Glyphs.menu[EDIT_MENU].append(newMenuItem)
-
-	# def showWindow_(self, sender):
-		"""Do something like show a window """
-		# pass
-
-	@objc.python_method
-	def __file__(self):
-		"""Please leave this method unchanged"""
-		return __file__
